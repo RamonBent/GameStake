4ff70f8ba3131c74a4423ef05111b5d6eec5a1dd
Ramon Bento <104931916+RamonBent@users.noreply.github.com>
2025-08-09 15:39:05 -0300

    Add files via upload

---
+++ backend/src/main/java/com/gamestake/backend/GameStakeApplication.java
@@ -0,0 +1,13 @@
+package com.gamestake.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStakeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gamestake/backend/GameStakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamestake.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
